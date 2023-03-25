a66dbaeda624ac8b090eb74d0dbd2a6ab6100e2f
HusseinF1991 <hus.f.06@gmail.com>
2023-03-25 14:52:31 +0300

    new commit

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,4 @@
-export const url='http://23.239.203.134:5000';
-// export const url='http://localhost:5000';
+export const url='http://localhost:5000';
 // export const socketIoUrl='http://localhost:5001';
 export const itemTypeImgUrl= `${url}/api/getItemImage/`
 export const shopImgUrl= `${url}/api/getShopImage/`
